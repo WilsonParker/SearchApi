a678cb002a22f8df94d147386b0ea2418367247e
dev9163 <dev9163@linkareer.com>
2021-07-09 17:44:10 +0900

    update api

--- app/Service/NaverSearchApi.php
+++ app/Service/NaverSearchApi.php
@@ -8,42 +8,57 @@ use GuzzleHttp\Client;
 
 class NaverSearchApi
 {
-    protected string $url = 'https://map.naver.com/v5/api/search';
-    // private string $url = 'https://map.naver.com/v5/api/search?caller=pcweb&query=%EC%84%9C%EC%9A%B8%EC%8B%9C%EC%B2%AD&type=all&searchCoord=126.97420399999992;37.566610300000235&page=1&displayCount=20&isPlaceRecommendationReplace=true&lang=ko';
+    protected string $url = 'https://map.naver.com';
+//     private string $url = 'https://map.naver.com/v5/api/search?caller=pcweb&query=%EC%84%9C%EC%9A%B8%EC%8B%9C%EC%B2%AD&type=all&searchCoord=126.97420399999992;37.566610300000235&page=1&displayCount=20&isPlaceRecommendationReplace=true&lang=ko';
     protected array $params = [];
     protected Client $client;
     protected int $page = 1;
     protected int $display = 20;
     protected string $lon = '126.97420399999992';
     protected string $lat = '37.566610300000235';
-    protected string $query = '';
+    protected string $query = '%EC%84%9C%EC%9A%B8%EC%8B%9C%EC%B2%AD';
 
     /**
      * NaverSearchApi constructor.
      */
     public function __construct()
     {
-        $this->client = new Client();
+        $this->client = new Client(
+            ['base_uri' => $this->url]
+        );
+        $this->initParams();
     }
 
     protected function initParams()
     {
         $this->params = [
-            'isPlaceRecommendationReplace' => 'true',
-            'lang' => 'ko',
             'caller' => 'pcweb',
+            'query' => "%EC%84%9C%EC%9A%B8%EC%8B%9C%EC%B2%AD",
+            // 'query' => $this->query,
             'type' => 'all',
-            'page' => $this->page,
-            'displayCount' => $this->display,
-            'searchCoord' => "$this->lat;$this->lon",
-            'query' => $this->query,
+            // 'page' => "$this->page",
+            'page' => 1,
+            'displayCount' => 20,
+            // 'displayCount' => "$this->display",
+            'searchCoord' => "126.97420399999992;37.566610300000235",
+            'isPlaceRecommendationReplace' => true,
+            // 'isPlaceRecommendationReplace' => 'true',
+            'lang' => 'ko',
         ];
     }
 
 
     public function call()
     {
-        $response = $this->client->request('GET', $this->url);
+        $response = $this->client->request('GET', $this->getSearchRoute(), [
+            'query' => $this->params
+        ]);
+        dump($response->getHeaders());
         dump($response->getBody()->getContents());
+    }
+
+    protected function getSearchRoute(): string
+    {
+        return '/v5/api/search';
     }
 }
